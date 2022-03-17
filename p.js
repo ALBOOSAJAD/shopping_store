f2ef53c1f15ef07058965edf7cf4f3fe405a1e51
ALBOOSAJAD <ALBOOSAJAD@gmail.com>
2022-03-17 18:31:18 +0330

    continued created html and js product

---
+++ p.js
@@ -0,0 +1,24 @@
+
+document.getElementById('btn').addEventListener('click',function () {
+   const name=  document.querySelector('#nam').value;
+   const price= document.querySelector('#pri').value;
+   const expline= document.querySelector('#tr').value;
+   const div= document.querySelector('#div').value;
+   
+   fetch('sdscdsdfdfgdfg',{
+       method:'post';
+       body: JSON.stringify({
+
+       })
+   }
+   )
+   .then(function (params) {
+       
+   })
+.then(function (params) {
+    
+})
+.catch(function (params) {
+    
+})
+})
